be54c66f9cb62fe65399fe94e2863ceb41cc6d30
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2025-03-23 00:58:08 +0300

    main.py: Fix docstrings and comments

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# main.py
+"""Main module for bot."""
+import matplotlib.pyplot as plt
 from support_analyzer import SupportAnalyzer
 from data_fetcher import DataFetcher
-import matplotlib.pyplot as plt
 
 def main():
-    # Veriyi çek
-    data_fetcher = DataFetcher(symbol="BTCUSDT")
+    """Main function to run the bot."""
+    # Fetch data
+    data_fetcher = DataFetcher(symbol="BTCUSDT", interval="15m", lookback="30d")
     df = data_fetcher.fetch_data()
-    
-    # Destek ve alım bölgesini hesapla
+
+    # Calculate support and buy zone
     analyzer = SupportAnalyzer(df, window=20)
-    support_levels = analyzer.find_support_levels()
-    buy_zone_lower, buy_zone_upper = analyzer.calculate_ideal_buy_zone()
-    
+    buy_zone_lower, buy_zone_upper = analyzer.calculate_mean_based_ideal_buy_zone()
+
     current_price = df["close"].iloc[-1]
-    
-    # Grafik çiz
+
+    # Draw plot
     plt.figure(figsize=(14,7))
     plt.plot(df["timestamp"], df["close"], label="Fiyat")
     plt.axhline(y=buy_zone_upper, color="g", linestyle="--", label="Alım Bölgesi (Üst Sınır)")
