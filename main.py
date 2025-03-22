4639d26c1a34d4d3f919436c0ca47db2476b1744
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2025-03-23 01:59:00 +0300

    Add plotter to the main module

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 """Main module for bot."""
-import matplotlib.pyplot as plt
+from plotter import PricePlotter
 from support_analyzer import SupportAnalyzer
 from data_fetcher import DataFetcher
+
 
 def main():
     """Main function to run the bot."""
     # Fetch data
-    data_fetcher = DataFetcher(symbol="BTCUSDT", interval="15m", lookback="30d")
+    data_fetcher = DataFetcher(
+        symbol="BTCUSDT", interval="15m", lookback="30d")
     df = data_fetcher.fetch_data()
 
     # Calculate support and buy zone
     analyzer = SupportAnalyzer(df, window=20)
-    buy_zone_lower, buy_zone_upper = analyzer.calculate_mean_based_ideal_buy_zone()
+    support_indices = analyzer.find_support_levels()
+    buy_zone = analyzer.calculate_mean_based_buy_zone()
 
     current_price = df["close"].iloc[-1]
 
     # Draw plot
-    plt.figure(figsize=(14,7))
-    plt.plot(df["timestamp"], df["close"], label="Fiyat")
-    plt.axhline(y=buy_zone_upper, color="g", linestyle="--", label="Alım Bölgesi (Üst Sınır)")
-    plt.axhline(y=buy_zone_lower, color="r", linestyle="--", label="Alım Bölgesi (Alt Sınır)")
-    plt.title(f"Alım Bölgesi: {buy_zone_lower:.2f} - {buy_zone_upper:.2f} | Şu Anki Fiyat: {current_price:.2f}")
-    plt.legend()
-    plt.show()
+    plotter = PricePlotter(df)
+    plotter.add_buy_zone(buy_zone)
+    plotter.add_support_levels(support_indices)
+    plotter.add_current_price(current_price)
+    plotter.customize_plot(
+        title=f"Buy Zone: {buy_zone[0]:.2f} - {buy_zone[1]:.2f} | Current Price: {current_price:.2f}"
+    )
+
+    plotter.show()  # Grafiği göster
+    # plotter.save("btc_chart2.png")  # Kaydetmek için
+
 
 if __name__ == "__main__":
     main()
